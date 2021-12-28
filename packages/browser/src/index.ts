347cc1ede3eccf65c45e4d765902bf37e5ef97a6
yoheinishikubo <yohei@me.com>
2021-12-28 20:49:23 +0900

    Update index.ts

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -6,10 +6,16 @@ import startRegistration from './methods/startRegistration';
 import startAuthentication from './methods/startAuthentication';
 import { browserSupportsWebauthn } from './helpers/browserSupportsWebauthn';
 import { platformAuthenticatorIsAvailable } from './helpers/platformAuthenticatorIsAvailable';
+import utf8StringToBuffer from '../helpers/utf8StringToBuffer';
+import bufferToBase64URLString from '../helpers/bufferToBase64URLString';
+import base64URLStringToBuffer from '../helpers/base64URLStringToBuffer';
 
 export {
   startRegistration,
   startAuthentication,
   browserSupportsWebauthn,
   platformAuthenticatorIsAvailable,
+  utf8StringToBuffer,
+  bufferToBase64URLString,
+  base64URLStringToBuffer
 };
